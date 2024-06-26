6527f61362a1c8e991202779113b9a9d6b2fbf82
Lucas Mathielo <mathielo74@gmail.com>
2024-06-26 20:48:16 +0200

    included inital legacy code

---
+++ NetworkOperatorPerformer/Network/NetworkOperatorPerformer.swift
@@ -0,0 +1,96 @@
+//
+//  NetworkOperatorPerformer.swift
+//  NetworkOperatorPerformer
+//
+//  Created by Lucas Mathielo on 26/06/24.
+//
+
+import Foundation
+import Network
+
+public class NetworkOperationPerformer {
+    private let networkMonitor: NetworkMonitor
+    private var timer: Timer?
+    private var closure: (() -> Void)?
+    
+    public init() {
+        self.networkMonitor = NetworkMonitor()
+    }
+    
+    /// Attempts to perform a network operation using the given `closure`, within the given `timeoutDuration`.
+    /// If the network is not accessible within the given `timeoutDuration`, the operation is not performed.
+    public func performNetworkOperation(
+        using closure: @escaping () -> Void,
+        withinSeconds timeoutDuration: TimeInterval
+    ) {
+        self.closure = closure
+        if self.networkMonitor.hasInternetConnection() {
+            closure()
+        } else {
+            tryPerformingNetworkOperation(withinSeconds: timeoutDuration)
+        }
+    }
+    
+    private func tryPerformingNetworkOperation(withinSeconds timeoutDuration: TimeInterval) {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(networkStatusDidChange(_:)),
+            name: .networkStatusDidChange,
+            object: nil
+        )
+        self.timer = Timer.scheduledTimer(withTimeInterval: timeoutDuration, repeats: false) { _ in
+            self.closure = nil
+            self.timer = nil
+            NotificationCenter.default.removeObserver(self)
+        }
+    }
+    
+    @objc func networkStatusDidChange(_ notification: Notification) {
+        guard
+            let connected = notification.userInfo?["connected"] as? Bool,
+            connected,
+            let closure
+        else {
+            return
+        }
+        closure()
+    }
+}
+
+private class NetworkMonitor {
+    private let monitor = NWPathMonitor()
+    
+    init() {
+        startMonitoring()
+    }
+    
+    private func startMonitoring() {
+        monitor.pathUpdateHandler = { _ in
+            NotificationCenter.default.post(
+                name: .networkStatusDidChange,
+                object: nil,
+                userInfo: ["connected": self.hasInternetConnection()]
+            )
+        }
+        monitor.start(queue: DispatchQueue(label: "NetworkMonitor"))
+    }
+    
+    func hasInternetConnection() -> Bool {
+        return monitor.currentPath.status == .satisfied
+    }
+}
+
+private extension Notification.Name {
+    static let networkStatusDidChange = Notification.Name("NetworkStatusDidChange")
+}
+
+
+// MARK: Snippet to use for running your code
+
+//let networkOperationClosure: () async -> SomeType = {
+//    // Long-lasting network operation.
+//    return result
+//}
+//let result = await NetworkOperationPerformer().perform(withinSeconds: 3) {
+//    return await networkOperationClosure()
+//}
