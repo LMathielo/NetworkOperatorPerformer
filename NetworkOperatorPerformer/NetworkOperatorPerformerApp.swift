775462745fafb2922295c9abfadd166d77584f97
Lucas Mathielo <mathielo74@gmail.com>
2024-06-26 20:44:29 +0200

    dump initial project

---
+++ NetworkOperatorPerformer/NetworkOperatorPerformerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkOperatorPerformerApp.swift
+//  NetworkOperatorPerformer
+//
+//  Created by Lucas Mathielo on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkOperatorPerformerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
